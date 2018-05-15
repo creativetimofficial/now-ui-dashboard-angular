abb4d78cdb95e90e3e4f291b8901f3b1cd0d2378
chelaruc <chelaru_ciprian@yahoo.com>
2018-05-15 17:29:01 +0300

    cleaned files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,6 @@ import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
 import {
   AgmCoreModule
 } from '@agm/core';
