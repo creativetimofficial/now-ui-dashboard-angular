07969a095cda79dd4cd5fd85b521a944ec5cde0a
digit81 <dargrab@protonmail.com>
2019-01-28 12:23:40 +0100

    Update to current version [28.01.2019] from https://www.creative-tim.com/product/now-ui-dashboard-angular

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -5,6 +5,7 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { Router, NavigationEnd, NavigationStart } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import PerfectScrollbar from 'perfect-scrollbar';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-admin-layout',
--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -62,30 +62,6 @@ export class NotificationsComponent implements OnInit {
         default:
         break;
       }
-
-
-      // $.notify({
-      //     icon: "notifications",
-      //     message: "Welcome to <b>Material Dashboard</b> - a beautiful freebie for every web developer."
-      //
-      // },{
-      //     type: type[color],
-      //     timer: 4000,
-      //     placement: {
-      //         from: from,
-      //         align: align
-      //     },
-      //     template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
-      //       '<button   type="button" aria-hidden="true" class="close " data-notify="dismiss">  <i class="material-icons">close</i></button>' +
-      //       '<i class="material-icons" data-notify="icon">notifications</i> ' +
-      //       '<span data-notify="title">{1}</span> ' +
-      //       '<span data-notify="message">{2}</span>' +
-      //       '<div class="progress" data-notify="progressbar">' +
-      //         '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
-      //       '</div>' +
-      //       '<a href="{3}" target="{4}" data-notify="url"></a>' +
-      //     '</div>'
-      // });
   }
   ngOnInit() {
   }
